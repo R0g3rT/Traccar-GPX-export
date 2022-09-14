525698210ef5b5e59fedacb9813c4cdf831267ac
R0g3rT <info@lippertz.org>
2022-09-14 16:19:03 +0200

    Fixed script error and added route name.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let btn = document.getElementById("btn");
 let date_now = new Date();
 
 // Login to Traccar with Data Request
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
     let server = document.querySelector("#vserver").value;
     let port = document.querySelector("#vport").value;
     let geraet = document.querySelector("#geraet").value;
@@ -17,7 +17,7 @@ btn.addEventListener("click", function() {
     sdaten.open("GET", server + ":" + port + "/api/positions?" + "deviceId=" + geraet + "&from=" + fdate + "Z&to=" + tdate + "Z");
     sdaten.setRequestHeader("Accept", "application/json");
     sdaten.setRequestHeader("Authorization", auth2);
-    sdaten.onreadystatechange = function() {
+    sdaten.onreadystatechange = function () {
         if (sdaten.readyState === XMLHttpRequest.DONE && sdaten.status === 200) {
             var daten = JSON.parse(sdaten.responseText);
             gpxsave(daten);
@@ -37,37 +37,44 @@ function gpxsave(data) {
     let fdate = document.querySelector("#fdate").value;
     let tdate = document.querySelector("#tdate").value;
     let save = document.querySelector("#save").value;
+    let info = document.querySelector("#info").value;
 
     var gpxdaten = "";
     gpxdaten +=
         //header
         '<?xml version="1.0" encoding="UTF-8" ?>\n' +
 
-        '<gpx version="1.1" creator="' + autor + ' "xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\n' +
+        '<gpx version="1.1" creator="' + autor + '" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\n' +
 
         //metadata
         '<metadata>\n' +
-            '<name>' + dname + '</name>\n' +
-            '<time>' + date_now.toISOString() + '</time>\n' +
-            '<desc>' + route + ' - Start: ' + fdate + ' - End: ' + tdate + '</desc>\n' +
+        '<name>' + dname + '</name>\n' +
+        '<time>' + date_now.toISOString() + '</time>\n' +
+        '<desc>' + route + ' - Start: ' + fdate + ' - End: ' + tdate + '</desc>\n' +
+        '<author>\n' +
+        '<name>' + autor + '</name>\n' +
+        '</author>\n' +
         '</metadata>\n\n' +
 
 
         //track
-        "<trk>\n<trkseg>\n" +
-        "\n"
+        '<trk>\n' +
+        '<name>' + route + '</name>\n' +
+        '<desc>' + info + '</desc>\n' +
+        '<trkseg>\n' +
+        '\n'
 
     for (i = 0; i < data.length; i++) {
         gpxdaten +=
             '<trkpt lat="' + data[i].latitude + '" lon="' + data[i].longitude + '">\n' +
-                '<ele>' + data[i].altitude + '</ele>\n' +
-                '<time>' + data[i].deviceTime.replace("+00:00", "") + '</time>\n' +
-                '<speed>' + data[i].speed + '</speed>\n' +
-                '<course>' + data[i].course + '</course>\n' +
+            '<ele>' + data[i].altitude + '</ele>\n' +
+            '<time>' + data[i].deviceTime.replace(".000+00:00", "") + "Z" + '</time>\n' +
+            '<speed>' + data[i].speed + '</speed>\n' +
+            '<course>' + data[i].course + '</course>\n' +
             '</trkpt>\n\n'
 
     }; +
-    "\n"
+        '\n'
     gpxdaten +=
         //end
         '</trkseg>\n' +
@@ -81,7 +88,7 @@ function gpxsave(data) {
         link.click();
     };
 
-    downloadURI("data:text/gpx," + gpxdaten, (dname + "-" + save + ".gpx"));
+    downloadURI("data:text/gpx," + gpxdaten, (save + ".gpx"));
 
 
 };
